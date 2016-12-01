d2b7ed5817e118ea271f2772fbfc4a63fc1d0f7a
Остап Коверко <net1552@elbank.com.ua>
2016-12-01 17:30:50 +0200

    Create Testimations caption

---
+++ js/testimonials.js
@@ -0,0 +1,16 @@
+$("#testimonials_slider").owlCarousel({
+    autoPlay: true, //Set AutoPlay to 3 seconds
+    navigation : false, // Show next and prev buttons
+    slideSpeed : 1000,
+    paginationSpeed : 1000,
+    stopOnHover : true,
+    singleItem:true,
+    responsive:true
+});
+
+///////////////////////////////////////////
+///////////////// Loading /////////////////
+///////////////////////////////////////////
+// $(window).load(function () {
+//       $("#testimonials_slider").show();
+// });
